564b3a138226a7f8fab0fd6e09c825b7a00b33b5
Daniël de Kok <me@danieldk.eu>
2025-01-26 18:12:00 +0100

    Add input dialog (still unused)

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,22 @@
 use std::collections::HashMap;
 
-use color_eyre::Result;
+use color_eyre::{owo_colors::colors::xterm::Brown, Result};
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use ratatui::{
     buffer::Buffer,
-    layout::{Constraint, Layout, Margin, Rect},
+    layout::{Constraint, Layout, Margin, Position, Rect},
     style::{
-        palette::tailwind::{BLUE, SLATE},
+        palette::{
+            material::PINK,
+            tailwind::{BLUE, FUCHSIA, SLATE},
+        },
         Color, Modifier, Style, Stylize,
     },
     symbols,
     text::{Line, Span},
     widgets::{
-        Block, Borders, List, ListState, Padding, Paragraph, Scrollbar, ScrollbarState,
-        StatefulWidget, Widget, Wrap,
+        Block, Borders, List, ListState, Padding, Paragraph, ScrollDirection, Scrollbar,
+        ScrollbarState, StatefulWidget, Widget, Wrap,
     },
     DefaultTerminal, Frame,
 };
@@ -24,16 +27,105 @@ use crate::metadata::cmp_numeric_lexicographic;
 const TODO_HEADER_STYLE: Style = Style::new().fg(SLATE.c100).bg(BLUE.c800);
 const NORMAL_ROW_BG: Color = SLATE.c950;
 const ALT_ROW_BG_COLOR: Color = SLATE.c900;
-const SELECTED_STYLE: Style = Style::new().bg(SLATE.c800).add_modifier(Modifier::BOLD);
+//const SELECTED_STYLE: Style = Style::new().bg(SLATE.c800).add_modifier(Modifier::BOLD);
+const SELECTED_STYLE: Style = Style::new().bg(PINK.c600).add_modifier(Modifier::BOLD);
 const TEXT_FG_COLOR: Color = SLATE.c200;
 
 #[derive(Debug, Default)]
+struct InputState {
+    input: String,
+    character_index: usize,
+}
+
+impl InputState {
+    const fn new() -> Self {
+        Self {
+            input: String::new(),
+            character_index: 0,
+        }
+    }
+
+    fn text(&self) -> &str {
+        &self.input
+    }
+
+    fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_right);
+    }
+
+    fn enter_char(&mut self, new_char: char) {
+        let index = self.byte_index();
+        self.input.insert(index, new_char);
+        self.move_cursor_right();
+    }
+
+    /// Returns the byte index based on the character position.
+    ///
+    /// Since each character in a string can be contain multiple bytes, it's necessary to calculate
+    /// the byte index based on the index of the character.
+    fn byte_index(&self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.input.len())
+    }
+
+    fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+            // Method "remove" is not used on the saved text for deleting the selected char.
+            // Reason: Using remove on String works on bytes instead of the chars.
+            // Using remove would require special care because of char boundaries.
+
+            let current_index = self.character_index;
+            let from_left_to_current_index = current_index - 1;
+
+            // Getting all characters before the selected character.
+            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+            // Getting all characters after selected character.
+            let after_char_to_delete = self.input.chars().skip(current_index);
+
+            // Put all characters together except the selected one.
+            // By leaving the selected one out, it is forgotten and therefore deleted.
+            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.input.chars().count())
+    }
+
+    fn reset_cursor(&mut self) {
+        self.character_index = 0;
+    }
+}
+
+#[derive(Debug, Default, Eq, PartialEq)]
+#[non_exhaustive]
+enum UiState {
+    #[default]
+    Browse,
+    Filter,
+    Quit,
+}
+
+#[derive(Debug, Default)]
 pub struct App {
+    cursor_position: Option<Position>,
     tensor_names: Vec<String>,
     tensors: HashMap<String, TensorInfo>,
     tensor_state: ListState,
     tensor_scrollbar_state: ScrollbarState,
-    running: bool,
+    state: UiState,
+    filter_state: InputState,
 }
 
 impl App {
@@ -44,19 +136,25 @@ impl App {
         let scroll_len = tensor_names.len();
 
         Self {
+            cursor_position: None,
+            filter_state: Default::default(),
             tensor_names,
             tensors,
             tensor_state: Default::default(),
             tensor_scrollbar_state: ScrollbarState::new(scroll_len),
-            running: false,
+            state: UiState::Browse,
         }
     }
 
     /// Run the application's main loop.
     pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
-        self.running = true;
-        while self.running {
-            terminal.draw(|frame| frame.render_widget(&mut self, frame.area()))?;
+        while !matches!(self.state, UiState::Quit) {
+            terminal.draw(|frame| {
+                frame.render_widget(&mut self, frame.area());
+                if let Some(cursor_position) = self.cursor_position {
+                    frame.set_cursor_position(cursor_position);
+                }
+            })?;
             if let Event::Key(key) = event::read()? {
                 self.handle_key(key);
             };
@@ -68,18 +166,48 @@ impl App {
         if key.kind != KeyEventKind::Press {
             return;
         }
-        match key.code {
-            KeyCode::Char('q') | KeyCode::Esc => self.quit(),
-            //KeyCode::Char('h') | KeyCode::Left => self.select_none(),
-            KeyCode::Char('j') | KeyCode::Down => self.select_next(),
-            KeyCode::Char('k') | KeyCode::Up => self.select_previous(),
-            KeyCode::Char('g') | KeyCode::Home => self.select_first(),
-            KeyCode::Char('G') | KeyCode::End => self.select_last(),
-            //KeyCode::Char('l') | KeyCode::Right | KeyCode::Enter => {
-            //    self.toggle_status();
-            // }
-            _ => {}
+
+        match self.state {
+            UiState::Browse => match key.code {
+                KeyCode::Char('q') | KeyCode::Esc => self.quit(),
+                //KeyCode::Char('h') | KeyCode::Left => self.select_none(),
+                KeyCode::Char('j') | KeyCode::Down => self.select_next(),
+                KeyCode::Char('k') | KeyCode::Up => self.select_previous(),
+                KeyCode::Char('g') | KeyCode::Home => self.select_first(),
+                KeyCode::Char('G') | KeyCode::End => self.select_last(),
+                KeyCode::Char('/') => self.state = UiState::Filter,
+                KeyCode::PageDown => self.page_down(),
+                KeyCode::PageUp => self.page_up(),
+                //KeyCode::Char('l') | KeyCode::Right | KeyCode::Enter => {
+                //    self.toggle_status();
+                // }
+                _ => {}
+            },
+            UiState::Filter => match key.code {
+                KeyCode::Enter => self.state = UiState::Browse,
+                KeyCode::Esc => self.state = UiState::Browse,
+                KeyCode::Char(to_insert) => self.filter_state.enter_char(to_insert),
+                KeyCode::Backspace => self.filter_state.delete_char(),
+                KeyCode::Left => self.filter_state.move_cursor_left(),
+                KeyCode::Right => self.filter_state.move_cursor_right(),
+                _ => {}
+            },
+            UiState::Quit => {}
         }
+    }
+
+    fn render_filter(&mut self, area: Rect, buf: &mut Buffer) {
+        self.cursor_position = None;
+        if self.state != UiState::Filter {
+            return;
+        }
+        let filter =
+            Paragraph::new(self.filter_state.text()).block(Block::bordered().title("Filter"));
+        Widget::render(filter, area, buf);
+        self.cursor_position = Some(Position::new(
+            area.x + self.filter_state.character_index as u16 + 1,
+            area.y + 1,
+        ));
     }
 
     fn render_list(&mut self, area: Rect, buf: &mut Buffer) {
@@ -154,8 +282,22 @@ impl App {
             .render(area, buf);
     }
 
+    fn page_down(&mut self) {
+        self.tensor_state.scroll_down_by(10);
+        if let Some(position) = self.tensor_state.selected() {
+            self.tensor_scrollbar_state = self.tensor_scrollbar_state.position(position);
+        }
+    }
+
+    fn page_up(&mut self) {
+        self.tensor_state.scroll_up_by(10);
+        if let Some(position) = self.tensor_state.selected() {
+            self.tensor_scrollbar_state = self.tensor_scrollbar_state.position(position);
+        }
+    }
+
     fn quit(&mut self) {
-        self.running = false;
+        self.state = UiState::Quit;
     }
 
     fn select_first(&mut self) {
@@ -188,10 +330,20 @@ impl Widget for &mut App {
         ])
         .areas(area);
 
-        let [list_area, item_area] =
+        let [select_area, detail_area] =
             Layout::horizontal([Constraint::Fill(1), Constraint::Fill(1)]).areas(main_area);
 
+        let [list_area, filter_area] =
+            match self.state {
+                UiState::Browse => Layout::vertical([Constraint::Fill(1), Constraint::Length(0)])
+                    .areas(select_area),
+                UiState::Filter => Layout::vertical([Constraint::Fill(1), Constraint::Length(3)])
+                    .areas(select_area),
+                UiState::Quit => unreachable!(),
+            };
+
         self.render_list(list_area, buf);
-        self.render_selected_item(item_area, buf);
+        self.render_filter(filter_area, buf);
+        self.render_selected_item(detail_area, buf);
     }
 }
